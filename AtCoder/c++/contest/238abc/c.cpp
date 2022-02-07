86cc6ff7bacc18e9ec484ebcb36957fefdebc679
Ozaki Yohei <yohei@OzakinoMacBook-Air.local>
2022-02-07 22:49:14 +0900

    2/7

--- AtCoder/c++/contest/238abc/c.cpp
+++ AtCoder/c++/contest/238abc/c.cpp
@@ -5,17 +5,25 @@ using namespace atcoder;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define rrep(i,a,b) for(int i = a; i >= b; i--)
 using ll = long long;
+using mint = modint998244353;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
+mint sum(ll x) {
+    return mint(1+x)*x/2;
+}
+
 int main() {
-    int n, q;
-    cin >> n >> q;
-    vector<int> l(q), r(q);
-
-    rep(i,q) cin >> l[i] >> r[i];
-
-    if(n == q) cout << "Yes" << endl;
-    else cout << "No" << endl;
+    ll n;
+    cin >> n;
+    ll l = 1;
+    mint ans;
+    while(l <= n) {
+        ll r = l*10;
+        ll len = min(n+1, r) - l;
+        ans += sum(len);
+        l *= 10;
+    }
+    cout << ans.val() << endl;
     return 0;
 }
