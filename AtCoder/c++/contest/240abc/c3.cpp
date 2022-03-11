234d69e45f18efc3efb0af48fa5a96e52b4b3faf
Ozaki Yohei <yohei@OzakinoMacBook-Air.local>
2022-03-11 10:41:43 +0900

    3/11 1

--- AtCoder/c++/contest/240abc/c3.cpp
+++ AtCoder/c++/contest/240abc/c3.cpp
@@ -11,5 +11,25 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 // dp
 
 int main() {
+    int n, X;
+    vector<int> a, b;
+    cin >> n >> X;
+    a = vector<int>(n);
+    b = vector<int>(n);
+
+    vector<vector<bool>> dp(n+1, vector<bool>(X+101, false));
+    dp[0][0] = true;
+    rep(i,n) {
+        rep(x,X+1) {
+            if(dp[i][x]) {
+                dp[i+1][x+a[i]] = true;
+                dp[i+1][x+b[i]] = true;
+            }
+        }
+    }
+
+    rep(i,n) cin >> a[i] >> b[i];
+    if(dp[n][X]) cout << "Yes" << endl;
+    else cout << "No" << endl;
     return 0;
 }
