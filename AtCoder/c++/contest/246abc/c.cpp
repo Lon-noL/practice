0edee90bbe0c398d0ea6878a7e3a3abe1e129dd8
“Yohei <ozoz0712@gmail.com>
2022-04-07 17:40:56 +0900

    4/7

--- AtCoder/c++/contest/246abc/c.cpp
+++ AtCoder/c++/contest/246abc/c.cpp
@@ -7,5 +7,29 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main() {
+    ll n, k, x;
+    cin >> n >> k >> x;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
+
+    ll ans = 0;
+    rep(i,n) ans += a[i];
+
+    ll used = 0;
+    rep(i,n) used += a[i] / x;
+    used = min(used, k);
+    k -= used;
+    ans -= used * x;
+
+    rep(i,n) a[i] %= x;
+    sort(a.begin(), a.end());
+
+    for(int i = n-1; i >= 0; i--) {
+        if(k == 0) break;
+        ans -= a[i];
+        k--;
+    }
+
+    cout << ans << endl;
     return 0;
 }
