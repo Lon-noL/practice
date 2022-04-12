7561fc6c3a42489d89c970a39aa68c563728b19e
“Yohei <ozoz0712@gmail.com>
2022-04-12 23:29:59 +0900

    4/12

---
+++ AtCoder/contest/247abc/d3.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int Q;
+  cin >> Q;
+  queue<pair<int, int>> q;
+  for (int i = 0; i < Q; i++){
+    int t;
+    cin >> t;
+    if (t == 1){
+      int x, c;
+      cin >> x >> c;
+      q.push(make_pair(x, c));
+    }
+    if (t == 2){
+      int c;
+      cin >> c;
+      long long ans = 0;
+      while (!q.empty()){
+        if (q.front().second > c){
+          q.front().second -= c;
+          ans += (long long) q.front().first * c;
+          break;
+        } else {
+          ans += (long long) q.front().first * q.front().second;
+          c -= q.front().second;
+          q.pop();
+        }
+      }
+      cout << ans << endl;
+    }
+  }
+}
