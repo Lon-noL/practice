6554f53eaf44b484170f1e87664deb02aebca975
“Yohei <ozoz0712@gmail.com>
2022-06-06 22:42:17 +0900

    6/6

--- AtCoder/contest/254abc/c.cpp
+++ AtCoder/contest/254abc/c.cpp
@@ -9,23 +9,23 @@ using P = pair<int,int>;
 int main() {
     int n, k;
     cin >> n >> k;
-    vector<int> a(n+1), A(n+1), dif(n+1);
-    for(int i = 1; i <= n; i++){
-        cin >> a[i];
-        A[i] = a[i];
+    vector<int> a(n);
+    rep(i,n) cin >> a[i];
+    for(int i = 0; i < k; i++){
+        vector<int> b;
+        for(int j = i; j < n; j += k){
+            b.push_back(a[j]);
+        }
+        sort(b.begin(), b.end());
+        for(int j = i; j < n; j += k){
+            a[j] = b[j /k];
+        }
     }
-    int mx = n - k;
-    sort(A.begin(), A.end());
-    for(int i = 1; i <= n; i++){
-        if(a[i] != A[i]) dif[i]++;
+    bool ok = true;
+    for(int i = 1; i < n; i++){
+        if(a[i] < a[i-1]) ok = false;
     }
-    for(int i = 1; i <= n; i++){
-
-    }
-
-
-
-    if(a == A) cout << "Yes" << endl;
+    if(ok) cout << "Yes" << endl;
     else cout << "No" << endl;
     return 0;
 }
